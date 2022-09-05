a67bcfaad31022a8e100ccb97c458d14aa7a3267
박신욱 <kkkbbnn@naver.com>
2022-09-05 19:46:16 +0900

    9-5
    수정

--- Assets/Script/System Script/SoundSystem.cs
+++ Assets/Script/System Script/SoundSystem.cs
@@ -8,6 +8,20 @@ public class SoundSystem : MonoBehaviour
 
     public static SoundSystem instance; // 전역
 
+    private void Awake() // 이벤트 함수 9-5
+    {
+        // 내가 찾는 오브젝트의 타입이 SoundSystem 이라면
+        // var 자료형 추론 (어떤 타입이든 다 저장이 되는 자료형입니다.)
+        var soundObject = FindObjectsOfType<SoundSystem>();
+
+        // soundObject의 갯수가 한 개 라면
+        if(soundObject.Length > 1) // 비교
+        {
+            // 자기 자신을 파괴해서 오직 하나의 SoundSystem 오브젝트만 남길 수 있도록 합니다.
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
         if(instance == null)
